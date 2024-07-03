767027e00cd72adffb8fc8fd840a5619023fdc62
positiveWand <singbhang@gmail.com>
2024-07-03 20:49:14 +0900

    [modify] Address 임베디드 클래스 생성자 추가

--- src/main/java/my/practice/jpashop/domain/Address.java
+++ src/main/java/my/practice/jpashop/domain/Address.java
@@ -9,4 +9,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { // 리플렉션을 위한 기본생성자
+    }
+    
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
