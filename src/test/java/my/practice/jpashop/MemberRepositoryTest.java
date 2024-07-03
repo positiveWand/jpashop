863946d208ab6a40f9e2fdaa42b077f83088be41
positiveWand <singbhang@gmail.com>
2024-07-03 19:50:19 +0900

    [test] MemberRepository 테스트 추가

---
+++ src/test/java/my/practice/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package my.practice.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional // SpringBoot - 테스트 케이스에서 트랜잭션 사용 시 테스트 후 롤백 진행
+    @Rollback(false)
+    public void 멤버_저장_테스트() throws Exception {
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member);
+    }
+}
