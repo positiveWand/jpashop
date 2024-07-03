4f9949ceb90338a719c7c88f6a084857076b551e
positiveWand <singbhang@gmail.com>
2024-07-03 19:49:23 +0900

    [init] 프로젝트 시작

---
+++ src/test/java/my/practice/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package my.practice.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
