b38c0ed08c4d02ce46b0c38e1f35f215d18810fb
positiveWand <singbhang@gmail.com>
2024-07-03 21:13:03 +0900

    [add] 연관관계 편의 메소드 및 영속성 전이(cascade) 설정

--- src/main/java/my/practice/jpashop/domain/Category.java
+++ src/main/java/my/practice/jpashop/domain/Category.java
@@ -31,4 +31,9 @@ public class Category {
 
     @OneToMany(mappedBy = "parent")
     private List<Category> children = new ArrayList<>();
+
+    public void addChildCategory(Category child) {
+        this.children.add(child);
+        child.setParent(this);
+    }
 }
