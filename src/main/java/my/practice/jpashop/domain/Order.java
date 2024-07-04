3d367736a2796f9b84ec017f3a03078f0e0037c4
positiveWand <singbhang@gmail.com>
2024-07-04 17:16:38 +0900

    [add] 주문, 주문상품 관련 도메인 로직 추가

--- src/main/java/my/practice/jpashop/domain/Order.java
+++ src/main/java/my/practice/jpashop/domain/Order.java
@@ -46,4 +46,43 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==팩토리 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem: orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 메서드==//
+    // 주문 취소
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem: orderItems) {
+           orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+//        int totalPrice = 0;
+//        for(OrderItem orderItem: orderItems) {
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
 }
