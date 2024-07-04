d634185865f5d58ad61cdbf57cf6f9501efab226
positiveWand <singbhang@gmail.com>
2024-07-04 15:19:06 +0900

    [add] 상품 재고 관련 도메인 로직 추가 + 도메인 예외 추가

--- src/main/java/my/practice/jpashop/domain/item/Item.java
+++ src/main/java/my/practice/jpashop/domain/item/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import my.practice.jpashop.domain.Category;
+import my.practice.jpashop.domain.exception.NotEnoughStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,4 +25,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 재고 늘리기
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 줄이기
+    public void removeStock(int quantity) {
+        int remainStock = this.stockQuantity - quantity;
+        if(remainStock < 0) {
+            throw new NotEnoughStockException("stock cannot be below zero");
+        }
+
+        this.stockQuantity = remainStock;
+    }
 }
