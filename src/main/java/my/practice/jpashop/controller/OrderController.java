2bcd3d8eac1375c504751cf2a3c260612a34f0d6
positiveWand <singbhang@gmail.com>
2024-07-05 16:49:05 +0900

    [add] OrderController 추가, 상품 주문 기능 추가

---
+++ src/main/java/my/practice/jpashop/controller/OrderController.java
@@ -0,0 +1,42 @@
+package my.practice.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import my.practice.jpashop.domain.Member;
+import my.practice.jpashop.domain.item.Item;
+import my.practice.jpashop.service.ItemService;
+import my.practice.jpashop.service.MemberService;
+import my.practice.jpashop.service.OrderService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
